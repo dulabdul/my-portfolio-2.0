11773219ca6c6cae232f277fa14ef62f845a93cf
dulabdul <dul.abdoel21@gmail.com>
2023-04-04 19:36:58 +0700

    configure SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,35 @@
 import '@/styles/globals.css';
+import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
+  let title = 'Home | Portfolio';
+  let description = 'Website portfolio created by Abdulrahman.';
+  let url = 'https://ar-portofolio.my.id/';
+  let imageUrl = 'https://source.unsplash.com/random/?computer';
+  const configSEO = {
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      type: 'website',
+      locale: 'en_IE',
+      url: url,
+      siteName: 'SiteName',
+      images: [
+        {
+          url: imageUrl,
+          width: 800,
+          height: 600,
+          alt: title,
+          type: 'image/jpeg',
+        },
+      ],
+    },
+  };
   return (
     <ThemeProvider attribute='class'>
+      <DefaultSeo {...configSEO} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
